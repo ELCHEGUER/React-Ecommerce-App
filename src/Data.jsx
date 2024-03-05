54234f671cb0b9c0b06a46af094a0772fc00e41a
ELCHEGUER <elcheguerayoub35@gmail.com>
2024-03-05 12:50:25 +0100

    Edit the file Data

--- src/Data.jsx
+++ src/Data.jsx
@@ -3,56 +3,56 @@ const DATA = [
         id : 0,
         title : "IPhone 11",
         price : 750,
-        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        desc : "The iPhone 12 Pro is a premium smartphone from Apple, boasting a sleek design and powerful performance. Its 6.1-inch Super Retina XDR display offers stunning visuals with HDR support, making everything from photos to videos look vibrant and lifelike. Powered by the A14 Bionic chip, the iPhone 12 Pro delivers blazing-fast performance for seamless multitasking, gaming, and productivity. ",
         img : "/assets/images/products/iphone11.png"
     },
     {
         id : 1,
         title : "IPhone 11 Pro Max",
         price : 800,
-        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        desc : "The iPhone 12 Pro is a premium smartphone from Apple, boasting a sleek design and powerful performance. Its 6.1-inch Super Retina XDR display offers stunning visuals with HDR support, making everything from photos to videos look vibrant and lifelike. Powered by the A14 Bionic chip, the iPhone 12 Pro delivers blazing-fast performance for seamless multitasking, gaming, and productivity. ",
         img : "/assets/images/products/iphone11promax.png"
     },
     {
         id : 2,
         title : "IPhone 12 Mini",
         price : 950,
-        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        desc : "The iPhone 12 Pro is a premium smartphone from Apple, boasting a sleek design and powerful performance. Its 6.1-inch Super Retina XDR display offers stunning visuals with HDR support, making everything from photos to videos look vibrant and lifelike. Powered by the A14 Bionic chip, the iPhone 12 Pro delivers blazing-fast performance for seamless multitasking, gaming, and productivity. ",
         img : "/assets/images/products/iphone12mini.png"
     },
     {
         id : 3,
         title : "IPhone 12",
         price : 1050,
-        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        desc : "The iPhone 12 Pro is a premium smartphone from Apple, boasting a sleek design and powerful performance. Its 6.1-inch Super Retina XDR display offers stunning visuals with HDR support, making everything from photos to videos look vibrant and lifelike. Powered by the A14 Bionic chip, the iPhone 12 Pro delivers blazing-fast performance for seamless multitasking, gaming, and productivity. ",
         img : "/assets/images/products/iphone12.png"
     },
     {
         id : 4,
         title : "IPhone 12 Pro",
         price : 1150,
-        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        desc : "The iPhone 12 Pro is a premium smartphone from Apple, boasting a sleek design and powerful performance. Its 6.1-inch Super Retina XDR display offers stunning visuals with HDR support, making everything from photos to videos look vibrant and lifelike. Powered by the A14 Bionic chip, the iPhone 12 Pro delivers blazing-fast performance for seamless multitasking, gaming, and productivity.",
         img : "/assets/images/products/iphone12pro.png"
     },
     {
         id : 5,
         title : "IPhone 12 Pro Max",
         price : 1250,
-        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        desc : "The iPhone 12 Pro Max is a flagship smartphone from Apple, renowned for its cutting-edge technology and premium design. Featuring a stunning 6.7-inch Super Retina XDR display, it delivers vibrant colors and deep blacks, perfect for immersive viewing experiences. Powered by Apple's A14 Bionic chip, the iPhone 12 Pro Max offers lightning-fast performance, whether you're multitasking, gaming, or editing photos and videos. Its advanced triple-camera system includes a wide, ultra-wide, and telephoto lens, allowing you to capture professional-quality photos and videos in any situation.",
         img : "/assets/images/products/iphone12promax.png"
     },
     {
         id : 6,
         title : "Redmi Note 11s",
         price : 1110,
-        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        desc : "The Redmi Note 11S is a stylish and feature-packed smartphone from Xiaomi's popular Redmi series. With its vibrant and immersive display, powered by a MediaTek or Qualcomm Snapdragon processor, this device delivers smooth performance for everyday tasks, gaming, and multimedia consumption.",
         img : "/assets/images/products/phon.jpg"
     },
     {
         id : 7,
         title : "EP03 Montre Intelligente",
         price : 125,
-        desc : "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+        desc : "A smartwatch is a wearable device that combines the functionality of a traditional wristwatch with advanced technological features. It typically includes a digital display screen that can show the time, date, and various other information such as notifications, messages, emails, and fitness data.",
         img : "/assets/images/products/montre smart.jpg"
     }
 ]
